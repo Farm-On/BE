3e4d2827c775de4186d3aa7ba458f7b112ec18fb
mmije0ng <victon777@naver.com>
2025-01-12 15:17:34 +0900

    [#3] Fix: 채팅 API 파라미터에 example 추가

--- src/main/java/com/backend/farmon/controller/ChatRoomController.java
+++ src/main/java/com/backend/farmon/controller/ChatRoomController.java
@@ -28,9 +28,9 @@ public class ChatRoomController {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공")
     })
     @Parameters({
-            @Parameter(name = "userId", description = "로그인한 유저의 아이디(pk)", required = true),
-            @Parameter(name = "read", description = "읽음 여부 필터링. 안 읽은 채팅방만 필터링 시에는 false, 이외에는 true 입니다.", required = true),
-            @Parameter(name = "page", description = "페이지 번호, 1부터 시작입니다.", required = true)
+            @Parameter(name = "userId", description = "로그인한 유저의 아이디(pk)", example = "1", required = true),
+            @Parameter(name = "read", description = "읽음 여부 필터링. 안 읽은 채팅방만 필터링 시에는 false, 이외에는 true 입니다.", example = "false", required = true),
+            @Parameter(name = "page", description = "페이지 번호, 1부터 시작입니다.", example = "1", required = true)
     })
     @GetMapping("/rooms/all")
     public ApiResponse<ChatResponse.ChatRoomListDTO> getChatRoomPage (@RequestParam(name = "userId") Long userId,
@@ -42,20 +42,20 @@ public class ChatRoomController {
     }
 
 
-    // 전문가 이름과 일치하는 전체 채팅 목록 조회
+    // 전문가 이름과 일치하는 채팅 목록 조회
     @Operation(
-            summary = "전문가 이름으로 검색하여 일치하는 전문가와의 전체 채팅 목록 조회 API",
-            description = "전문가 이름을 검색하여 일치하는 전체 채팅 목록을 조회하는 API이며, 페이징을 포함합니다. " +
+            summary = "전문가 이름으로 검색하여 일치하는 전문가와의 채팅 목록 조회 API",
+            description = "전문가 이름을 검색하여 일치하는 채팅 목록을 조회하는 API이며, 페이징을 포함합니다. " +
                     "유저 아이디, 읽음 여부 필터, 검색할 전문가 이름, 페이지 번호를 쿼리 스트링으로 입력해주세요."
     )
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공")
     })
     @Parameters({
-            @Parameter(name = "userId", description = "로그인한 유저의 아이디(pk)", required = true),
-            @Parameter(name = "read", description = "읽음 여부 필터링. 안 읽은 채팅방만 필터링 시에는 false, 이외에는 true 입니다.", required = true),
-            @Parameter(name = "expertName", description = "검색할 전문가 이름입니다.", required = true),
-            @Parameter(name = "page", description = "페이지 번호, 1부터 시작입니다.", required = true)
+            @Parameter(name = "userId", description = "로그인한 유저의 아이디(pk)", example = "1", required = true),
+            @Parameter(name = "read", description = "읽음 여부 필터링. 안 읽은 채팅방만 필터링 시에는 false, 이외에는 true 입니다.", example = "false", required = true),
+            @Parameter(name = "expertName", description = "검색할 전문가 이름입니다.", example = "김팜온", required = true),
+            @Parameter(name = "page", description = "페이지 번호, 1부터 시작입니다.", example = "1", required = true)
     })
     @GetMapping("/rooms/expert")
     public ApiResponse<ChatResponse.ChatRoomListDTO> getChatRoomPageByExpertName (@RequestParam(name = "userId") Long userId,
@@ -68,20 +68,20 @@ public class ChatRoomController {
     }
 
 
-    // 작물 이름으로 검색하여 일치하는 견적과 관련된 채팅 목록 조회
+    // 작물 이름으로 검색하여 일치하는 전문가와의 채팅 목록 조회
     @Operation(
-            summary = "작물 이름으로 검색하여 일치하는 견적과 관련된 채팅 목록 조회",
-            description = "작물 이름으로 검색하여 일치하는 견적과 관련된 채팅 목록을 조회하는 API이며 페이징을 포함합니다. " +
-                    "유저 아이디, 읽음 여부 필터, 검색할 작물 이름, 페이지 번호를 쿼리 스트링으로 입력해주세요."
+            summary = "작물 이름으로 검색하여 일치하는 전문가와의 채팅 목록 조회",
+            description = "작물 이름으로 검색하여 일치하는 해당 분야의 전문가와의 채팅 목록을 조회하는 API이며 페이징을 포함합니다. " +
+                    "유저 아이디, 읽음 여부 필터, 검색할 전문가의 작물 이름, 페이지 번호를 쿼리 스트링으로 입력해주세요."
     )
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공")
     })
     @Parameters({
-            @Parameter(name = "userId", description = "로그인한 유저의 아이디(pk)", required = true),
-            @Parameter(name = "read", description = "읽음 여부 필터링. 안 읽은 채팅방만 필터링 시에는 false, 이외에는 true 입니다., required = true"),
-            @Parameter(name = "cropsName", description = "검색할 작물 이름입니다.", required = true),
-            @Parameter(name = "page", description = "페이지 번호, 1부터 시작입니다.", required = true)
+            @Parameter(name = "userId", description = "로그인한 유저의 아이디(pk)", example = "1", required = true),
+            @Parameter(name = "read", description = "읽음 여부 필터링. 안 읽은 채팅방만 필터링 시에는 false, 이외에는 true 입니다.", example = "1", required = true),
+            @Parameter(name = "cropsName", description = "검색할 전문가의 작물 이름(쌀, 곡물 등)입니다.", example = "쌀", required = true),
+            @Parameter(name = "page", description = "페이지 번호, 1부터 시작입니다.", example = "1", required = true)
     })
     @GetMapping("/rooms/crops")
     public ApiResponse<ChatResponse.ChatRoomListDTO> getChatRoomPageByCropsName (@RequestParam(name = "userId") Long userId,
@@ -104,8 +104,8 @@ public class ChatRoomController {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공")
     })
     @Parameters({
-            @Parameter(name = "userId", description = "로그인한 유저의 아이디(pk)", required = true),
-            @Parameter(name = "chatRoomId", description = "입장하려는 채팅방의 아이디", required = true),
+            @Parameter(name = "userId", description = "로그인한 유저의 아이디(pk)", example = "1", required = true),
+            @Parameter(name = "chatRoomId", description = "입장하려는 채팅방의 아이디", example = "1", required = true),
     })
     @GetMapping("/room")
     public ApiResponse<ChatResponse.ChatMessageListDTO> enterChatRoom (@RequestParam(name = "userId") Long userId,
@@ -126,8 +126,8 @@ public class ChatRoomController {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200", description = "OK, 성공")
     })
     @Parameters({
-            @Parameter(name = "userId", description = "로그인한 유저의 아이디(pk)"),
-            @Parameter(name = "chatRoomId", description = "삭제하려는 채팅방의 아이디"),
+            @Parameter(name = "userId", description = "로그인한 유저의 아이디(pk)", example = "1", required = true),
+            @Parameter(name = "chatRoomId", description = "삭제하려는 채팅방의 아이디", example = "1", required = true),
     })
     @DeleteMapping("/room")
     public ApiResponse<ChatResponse.ChatRoomDeleteDTO> deleteChatRoom (@RequestParam(name = "userId") Long userId,
