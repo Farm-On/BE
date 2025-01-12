2ac1df62b836ffaf0e0947b5fe4c75449c5d092d
mmije0ng <victon777@naver.com>
2025-01-13 02:00:06 +0900

    [#20] Feat: 홈 화면 특정 검색어 삭제, 전체 검색어 삭제 API 추가

--- src/main/java/com/backend/farmon/controller/HomeController.java
+++ src/main/java/com/backend/farmon/controller/HomeController.java
@@ -1,7 +1,6 @@
 package com.backend.farmon.controller;
 
 import com.backend.farmon.apiPayload.ApiResponse;
-import com.backend.farmon.dto.chat.ChatResponse;
 import com.backend.farmon.dto.home.HomeResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -29,11 +28,45 @@ public class HomeController {
     })
     @Parameters({
             @Parameter(name = "userId", description = "로그인한 유저의 아이디(pk), 로그인 하지 않은 사용자 일 경우 입력하지 않아도 됩니다.", example = "1"),
-            @Parameter(name = "category", description = " 커뮤니티 카테고리 이름", example = "인기", required = true)
+            @Parameter(name = "category", description = "커뮤니티 카테고리 이름", example = "인기", required = true)
     })
     @GetMapping("")
     public ApiResponse<HomeResponse.HomePageDTO> getChatRoomPage (@RequestParam(name = "userId", required = false) Long userId,
                                                                   @RequestParam(name = "category") String category){
+        return null;
+    }
+
+    // 검색어 삭제 API
+    @Operation(
+            summary = "홈 화면에서 특정 검색어 삭제 API",
+            description = "홈 화면에서 특정 검색어를 삭제하는 API 입니다. 유저 아이디와 삭제할 검색어 이름을 쿼리 스트링으로 입력해 주세요."
+    )
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공")
+    })
+    @Parameters({
+            @Parameter(name = "userId", description = "로그인한 유저의 아이디(pk)", example = "1"),
+            @Parameter(name = "name", description = "삭제 할 검색어 이름", example = "병충해 관리")
+    })
+    @DeleteMapping("/search")
+    public ApiResponse<HomeResponse.HomePageDTO> deleteSearchName(@RequestParam(name = "userId") Long userId,
+                                                                  @RequestParam(name = "name") String searchName){
+        return null;
+    }
+
+    // 검색어 전체 삭제 API
+    @Operation(
+            summary = "홈 화면에서 사용자의 검색어를 전체 삭제 API",
+            description = "홈 화면에서 사용자의 검색어를 전체 삭제 API. 유저 아이디를 쿼리 스트링으로 입력해 주세요."
+    )
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공")
+    })
+    @Parameters({
+            @Parameter(name = "userId", description = "로그인한 유저의 아이디(pk)", example = "1")
+    })
+    @DeleteMapping("/search/all")
+    public ApiResponse<HomeResponse.HomePageDTO> deleteAllSearchName(@RequestParam(name = "userId") Long userId){
         return null;
     }
 }
