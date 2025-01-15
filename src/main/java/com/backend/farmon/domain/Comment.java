28998c72601a4616d4009f42f5bd0b21b613d0cd
jeonhanjoon <hjun230604@gmail.com>
2025-01-15 19:32:06 +0900

    Board게시판제외하고 모두제작

---
+++ src/main/java/com/backend/farmon/domain/Comment.java
@@ -0,0 +1,50 @@
+package com.backend.farmon.domain;
+
+import com.backend.farmon.domain.commons.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Table(name="Comment_Table")
+@Entity
+@Setter
+@Getter
+public class Comment extends BaseEntity {
+    // 수정까지 가능
+
+
+    @Id
+    @GeneratedValue
+    @Column(name="comment_id")
+    private Long id;
+    //id로 통일
+
+    private String commentContent;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="PARENT_ID")
+    private Comment parent;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    //삭제기능은 구현 안함
+
+    //일단 사용자 : 댓글 1:ㅜ
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    private User user;
+
+
+    //부모댓글과 대댓글
+    @OneToMany(mappedBy="parent",fetch=FetchType.LAZY,
+    cascade = {CascadeType.ALL},orphanRemoval = true)
+    private List<Comment> children=new ArrayList<>();
+
+
+
+}
