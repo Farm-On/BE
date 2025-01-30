c063fa336bcfeb1b2f1a6c738cc7d4798703c395
jeonhanjoon <hjun230604@gmail.com>
2025-01-30 14:10:21 +0900

    댓글 완성

--- src/main/java/com/backend/farmon/domain/Comment.java
+++ src/main/java/com/backend/farmon/domain/Comment.java
@@ -2,6 +2,8 @@ package com.backend.farmon.domain;
 
 import com.backend.farmon.domain.commons.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +14,8 @@ import java.util.List;
 @Entity
 @Setter
 @Getter
+@Builder
+@AllArgsConstructor
 public class Comment extends BaseEntity {
     // 수정까지 가능
 
@@ -46,5 +50,7 @@ public class Comment extends BaseEntity {
     private List<Comment> children=new ArrayList<>();
 
 
-
+    public Comment() {
+        
+    }
 }
