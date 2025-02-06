e37eefc09d1ef349559c04069b665f1260623150
mmije0ng <victon777@naver.com>
2025-02-06 23:34:30 +0900

    Fix: 병합 오류 해결

--- src/main/java/com/backend/farmon/repository/CropRepository/CropRepository.java
+++ src/main/java/com/backend/farmon/repository/CropRepository/CropRepository.java
@@ -1,6 +1,5 @@
 package com.backend.farmon.repository.CropRepository;
 
-import com.backend.farmon.domain.Area;
 import com.backend.farmon.domain.Crop;
 import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,11 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Set;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 public interface CropRepository extends JpaRepository<Crop, Long> {
     Optional<Crop> findByName(String name);
