1bd066da682fbff166c4be65557c29a017ce1c05
Hanjun2022 <166356744+Hanjun2022@users.noreply.github.com>
2025-02-15 20:00:02 +0900

    Update CommentRepository.java

--- src/main/java/com/backend/farmon/repository/CommentRepository/CommentRepository.java
+++ src/main/java/com/backend/farmon/repository/CommentRepository/CommentRepository.java
@@ -1,6 +1,7 @@
 package com.backend.farmon.repository.CommentRepository;
 
 import com.backend.farmon.domain.Comment;
+import com.backend.farmon.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -22,8 +23,21 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query("SELECT COALESCE(MAX(c.groupOrder), 0) FROM Comment c WHERE c.groupId = :groupId")
     Optional<Integer> findMaxGroupOrderByGroupId(@Param("groupId") Long groupId);
 
+    // 특정 original_comment_id와 post_id를 가진 댓글이 존재하는지 확인 (대댓글 중복 방지 용도)
+    boolean existsByOriginalCommentIdAndPostId(Long originalCommentId, Long postId);
 
+    // originalCommentId를 기준으로 관련 댓글들을 모두 조회합니다.
+    List<Comment> findAllByOriginalCommentId(Long originalCommentId);
+
+
+    List<Comment>findAllByGroupId(Long groupId);
+
+    // 부모댓글이 있는 찾기
     boolean existsByParentId(Long parentId);
 
+    @Query("SELECT MAX(c.groupId) FROM Comment c")
+    Optional<Long> findMaxGroupId();
 
+    // 특정 부모 댓글에 대댓글이 있는지 확인하는 메서드 추가
+    boolean existsByParentIdAndOriginalCommentId(Long parentId, Long originalCommentId);
 }
