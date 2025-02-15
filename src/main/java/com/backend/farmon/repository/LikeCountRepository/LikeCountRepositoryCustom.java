5dcc755ac87400a1e5fb4698822f78a0b314a2db
Hanjun2022 <166356744+Hanjun2022@users.noreply.github.com>
2025-02-15 20:00:59 +0900

    Update LikeCountRepositoryCustom.java

--- src/main/java/com/backend/farmon/repository/LikeCountRepository/LikeCountRepositoryCustom.java
+++ src/main/java/com/backend/farmon/repository/LikeCountRepository/LikeCountRepositoryCustom.java
@@ -5,8 +5,5 @@ import com.backend.farmon.domain.User;
 
 public interface LikeCountRepositoryCustom {
 
-    boolean checkLike(Post post, User user);
-    void like(Post post, User user);
-    void unlike(Post post, User user);
 
 }
