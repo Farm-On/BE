ab42ee13532f4057d6e3d96f2711c9d7c8db7655
Hanjun2022 <166356744+Hanjun2022@users.noreply.github.com>
2025-02-15 20:30:10 +0900

    Delete src/main/java/com/backend/farmon/repository/LikeCountRepository/LikeCountRepositoryCustom.java

--- src/main/java/com/backend/farmon/repository/LikeCountRepository/LikeCountRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.backend.farmon.repository.LikeCountRepository;
-
-import com.backend.farmon.domain.Post;
-import com.backend.farmon.domain.User;
-
-public interface LikeCountRepositoryCustom {
-
-
-}
