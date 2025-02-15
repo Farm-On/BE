7cd039b13df8c5596b6ea44b65ef2fa8a80e1d78
Hanjun2022 <166356744+Hanjun2022@users.noreply.github.com>
2025-02-15 19:51:09 +0900

    Update PostImg.java

--- src/main/java/com/backend/farmon/domain/PostImg.java
+++ src/main/java/com/backend/farmon/domain/PostImg.java
@@ -22,6 +22,8 @@ public class PostImg extends BaseEntity {
 
     private String originalFileName;
 
+    private Long originalPostId;
+
     // Img:Post = N:1
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
