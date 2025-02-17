5bdf67f87f44fcd79de5524eff772c2722d01e8c
Hanjun2022 <166356744+Hanjun2022@users.noreply.github.com>
2025-02-17 20:41:38 +0900

    Update AnswerRepository.java

--- src/main/java/com/backend/farmon/repository/AnswerRepository/AnswerRepository.java
+++ src/main/java/com/backend/farmon/repository/AnswerRepository/AnswerRepository.java
@@ -4,13 +4,8 @@ package com.backend.farmon.repository.AnswerRepository;
 import com.backend.farmon.domain.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-<<<<<<< HEAD
 import java.util.List;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
     List<Answer> findAllByPostId(Long postId);
-=======
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
->>>>>>> origin/develop
 }
